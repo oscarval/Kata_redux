fe1cb612fe5db4784b45827b3c71a30ef5266688
Alexis <entulinea.alexis@gmail.com>
2020-05-14 23:53:22 +0200

    Remove code commented

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import Controls from "./components/Controls/Controls";
 import TodoList from "./components/TodoList/TodoList";
-// import { connect } from "react-redux";
 
 function App(props) {
   return (
@@ -12,16 +11,5 @@ function App(props) {
     </div>
   );
 }
-
-// const mapStateToProps = (state) => ({ state: state });
-// const mapDispacthToProps = (dispacth) => ({
-//   addrandomTodo: () =>
-//     dispacth({
-//       type: "ADD_TODO",
-//       payload: "borra esta tarea!",
-//     }),
-// });
-
-// const connectedApp = connect(mapStateToProps, mapDispacthToProps)(App);
 
 export default App;
